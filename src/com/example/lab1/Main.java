1e765952eb625e79cc60ab4036fe6d204911cd74
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-09-07 10:01:43 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
+		System.out.println("21800179 김준현");
 		System.out.print("이름 입력 : ");
 		String name = s.next();
 		System.out.println("[" + name + "]님 환영합니다~");
